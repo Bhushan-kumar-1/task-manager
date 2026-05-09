e4943a57895a28b4410aefc54c3df4cb275e54aa
Bhushan kumar <157486198+Bhushan-kumar-1@users.noreply.github.com>
2026-05-09 09:17:11 +0530

    Add files via upload

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+function Dashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>MERN Team Task Manager Dashboard</h1>
+      <p>Welcome to the project.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
